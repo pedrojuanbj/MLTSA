c9ed42fe381283a8faf93c2818c2fc8db251074f
pedrojuanbj <pedrojuanbj@gmail.com>
2022-07-03 20:54:38 +0100

    Fixing bugs for documentation generation and a bit of housekeeping as well, changed some configuration to make easier install.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     classifiers=classifiers,
     keywords="MLTSA",
     packages=find_packages(),
-    install_requires=["numpy", "scipy", "matplotlib", "tqdm", "scikit-learn"]
+    install_requires=[]
 )
 
 #"numpy", "scipy", "matplotlib", "tqdm", "scikit-learn"
