56ace7b5b694afb028a1b5a0416aebb7d00ff062
ZhengxiangWei <ucapzwe@ucl.ac.uk>
2022-07-17 17:24:42 +0100

    Add docstring for RNN generator class

--- MLTSA_tensorflow/TF_2_RNN.py
+++ MLTSA_tensorflow/TF_2_RNN.py
@@ -14,9 +14,22 @@ from tensorflow.keras.layers import SimpleRNN, Dense, Dropout, GRU
 
 
 class build_MLP():
-
+"""
+Factory class to ensemble RNN models.
+"""
     def __init__(self, n_steps, n_features, n_labels, type="vanilla"):
+	"""
+	initialize the RNN generator class
 
+	:param n_steps: Number of steps of the input data, shape the input layer of the model
+	:type n_steps: int
+	:param n_features: Number of features of the input data, shape the input layer of the model
+	:type n_features: int
+	:param n_labels: Number of labels in the input data, shape the output layer of the model
+	:type n_labels: int
+	:param type: type of the RNN model to be ensembled, optional, default as 'vanilla'
+	:type type: str
+	"""
         self.type = type
         self.n_labels = n_labels
         self.n_features = n_features
@@ -32,7 +45,14 @@ class build_MLP():
         return model
 
     def vanilla(self, n_units=100, dropout=0.1):
+	"""
+	Classic RNN model with one hidden layer
 
+	:param n_units: Number of units of the hidden layer, optional, default as 100
+	:type n_units: int
+	:param dropout: Dropout rate of the dropout process, optional, default as 0.1
+	:type dropout: float
+	"""
         model = Sequential()
 
         # Add Simple RNN layer with 100 units
@@ -54,7 +74,17 @@ class build_MLP():
         return model
 
     def GRU(self, n_units=100, dropout=0.1, n_layers=5):
+	"""
+	Gated recurrent unit RNN model
 
+
+	:param n_units: Number of units of the hidden layer, optional, default as 100
+	:type n_units: int
+	:param dropout: Dropout rate of the dropout process, optional, default as 0.1
+	:type dropout: float
+	:param n_layers: Number of the hidden layer, optional, default as 5 #TODO: Check with pedro, this args is not called in the function here
+	:type n_layers: int
+	"""
         model = Sequential()
 
         # Add GRU layer with 100 units
